2021f20df2f1a505230ee13ad5e4ffcf3bdb5f51
macbook <bakhtiyar.amrahov@gmail.com>
2023-02-08 21:54:00 +0400

    ToastrJsNgxSpinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrOptions, ToastrPosition } from './services/ui/custom-toastr.service';
 // declare var $:any
 
 @Component({
@@ -8,6 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'E-CommerceAngular';
+  constructor(private toastr: CustomToastrService){
+    toastr.message("Helle","Salam",{
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition.BottomCenter
+    });
+  }
 }
 // $(document).ready(()=>{
 //   alert("salam")
---
+++ src/app/base/base.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+
+export class BaseComponent {
+
+  constructor(private spinner: NgxSpinnerService){
+
+  }
+
+  showSpinner(name: SpinnerType){
+    this.spinner.show(name);
+
+    setTimeout(()=>{
+      this.hideSpinner(name)
+    },1000)
+  }
+
+  hideSpinner(name: SpinnerType){
+    this.spinner.hide(name)
+  }
+
+}
+
+
+export enum SpinnerType{
+
+  BallAtom = "spinnerOne",
+  BallScale = "spinnerTwo",
+  BallSpinClockwiseFadeRoatting = "spinnerThree"
+}
