1558f63d54e8f9ea092935c305cd3cb93374582b
macbook <bakhtiyar.amrahov@gmail.com>
2023-02-10 01:54:44 +0400

    CorsTestJquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { CustomToastrService, ToastrMessageType, ToastrOptions, ToastrPosition } from './services/ui/custom-toastr.service';
-// declare var $:any
+declare var $:any
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,16 @@ import { CustomToastrService, ToastrMessageType, ToastrOptions, ToastrPosition }
 export class AppComponent {
   title = 'E-CommerceAngular';
   constructor(private toastr: CustomToastrService){
-    toastr.message("Helle","Salam",{
-      messageType: ToastrMessageType.Success,
-      position: ToastrPosition.BottomCenter
-    });
+    // toastr.message("Helle","Salam",{
+    //   messageType: ToastrMessageType.Success,
+    //   position: ToastrPosition.BottomCenter
+    // });
   }
 }
+
+$.get("http://localhost:5193/Product/id?id=07490583-684f-481d-9aa5-0601dc8d7b9a",(data: any) => {
+  console.log(data);
+});
 // $(document).ready(()=>{
 //   alert("salam")
 // })
