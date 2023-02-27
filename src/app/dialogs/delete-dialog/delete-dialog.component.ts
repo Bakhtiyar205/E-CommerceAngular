93779273a6e9f6002bd0e33cb2207c10fd286870
macbook <bakhtiyar.amrahov@gmail.com>
2023-02-28 01:12:18 +0400

    DeleteModification

---
+++ src/app/dialogs/delete-dialog/delete-dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-dialog',
+  templateUrl: './delete-dialog.component.html',
+  styleUrls: ['./delete-dialog.component.scss']
+})
+export class DeleteDialogComponent implements OnInit{
+
+  constructor(
+    public dialogRef: MatDialogRef<DeleteDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DeleteState,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  close(){
+    this.dialogRef.close();
+  }
+
+}
+
+
+export enum DeleteState{
+  Yes, 
+  No
+}
--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,12 @@
+import { throwDialogContentAlreadyAttachedError } from '@angular/cdk/dialog';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
+import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Directive({
@@ -10,8 +16,10 @@ export class DeleteDirective {
 
   constructor(private element: ElementRef, 
               private _renderer: Renderer2, 
-              private productService: ProductService,
-              private spinner: NgxSpinnerService) {
+              private httpClientService: HttpClientService,
+              private spinner: NgxSpinnerService,
+              public dialog: MatDialog,
+              private alertify: AlertifyService) {
                 const img = _renderer.createElement("img");
                 img.setAttribute("src","../assets/8324271_ui_essential_app_check_delete_icon (1).png");
                 img.setAttribute("style","cursor:pointer");
@@ -20,33 +28,60 @@ export class DeleteDirective {
                 _renderer.appendChild(element.nativeElement, img);
                }
 
-    @Input() id: string | undefined
+    @Input() id: string | undefined;
+    @Input() controller: string | undefined;
     @Output() callBack: EventEmitter<any> = new EventEmitter()
 
     @HostListener("click")
     async onclick(){
-      if(this.id){
-        
-        
-        const td: HTMLTableCellElement = this.element.nativeElement.parentElement;
-        const fadeEffect = setInterval(()=>{
-            if(!td.style.opacity){
-              td.style.opacity = '1';
-            }if(td.style.opacity>'0'){
-              td.style.opacity = "0"
-            }else{
-              clearInterval(fadeEffect);
-            }
-        }, 200)
-
-        this.spinner.show(SpinnerType.BallAtom)
-        await this.productService.delete(this.id).then(()=>{
-          this.spinner.show(SpinnerType.BallAtom).then(()=>{
-             this.callBack.emit();
-          })
-        });
-      }
-
-    }      
+        this.openDialog(async ()=> {
+          if(this.id){
+            const td: HTMLTableCellElement = this.element.nativeElement.parentElement;
+            const fadeEffect = setInterval(()=>{
+                if(!td.style.opacity){
+                  td.style.opacity = '1';
+                }if(td.style.opacity>'0'){
+                  td.style.opacity = "0"
+                }else{
+                  clearInterval(fadeEffect);
+                }
+            }, 200)
+    
+            this.spinner.show(SpinnerType.BallAtom)
+            await this.httpClientService.delete({
+              controller: this.controller
+            }, this.id).subscribe(()=>{
+              this.spinner.show(SpinnerType.BallAtom)
+            }, ()=>{
+              this.callBack.emit();
+              this.alertify.message("Product is deleted succesfully.",{
+                dismissOthers: true,
+                messageType: MessageType.Success,
+                position: Position.TopRight
+              })
+            }),(errorResponse: HttpErrorResponse)=>{
+              this.spinner.hide();
+              this.alertify.message("There is something wrong",{
+                dismissOthers: true,
+                messageType: MessageType.Error,
+                position: Position.TopRight
+              })
+          }
+        }
+      })
+    }  
+    
+    openDialog(afterClosed: any): void {
+      const dialogRef = this.dialog.open(DeleteDialogComponent, {
+        width:'250px',
+        data: DeleteState.Yes,
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        if(result == DeleteState.Yes){
+          afterClosed();
+        }
+      });
+    }
   }
 
